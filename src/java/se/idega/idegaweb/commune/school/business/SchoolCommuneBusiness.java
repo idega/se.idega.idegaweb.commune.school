e267c6b80872c125c4148cb47d9cfe3fd7d0273a
Thorhallur Helgason <laddi@idega.com>
2002-10-13 09:53:23 +0000

    Added method to get previous school season.

--- src/java/se/idega/idegaweb/commune/school/business/SchoolCommuneBusiness.java
+++ src/java/se/idega/idegaweb/commune/school/business/SchoolCommuneBusiness.java
@@ -4,6 +4,7 @@ import javax.ejb.*;
 
 public interface SchoolCommuneBusiness extends com.idega.business.IBOService,com.idega.block.process.business.CaseBusiness
 {
+ public com.idega.block.school.data.SchoolSeason getPreviousSchoolSeason(com.idega.block.school.data.SchoolSeason p0)throws java.rmi.RemoteException, java.rmi.RemoteException;
  public com.idega.block.school.business.SchoolBusiness getSchoolBusiness()throws java.rmi.RemoteException, java.rmi.RemoteException;
  public se.idega.idegaweb.commune.school.business.SchoolChoiceBusiness getSchoolChoiceBusiness()throws java.rmi.RemoteException, java.rmi.RemoteException;
  public com.idega.block.school.business.SchoolClassBusiness getSchoolClassBusiness()throws java.rmi.RemoteException, java.rmi.RemoteException;
