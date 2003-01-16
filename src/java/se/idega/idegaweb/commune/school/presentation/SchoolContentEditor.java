bb7b3f7fc27fe2da2e6afc44b0f85b9ed0c46137
Grimur Jonsson <gimmi@idega.com>
2003-01-16 12:21:52 +0000

    Extends SchoolContentEditor from com

---
+++ src/java/se/idega/idegaweb/commune/school/presentation/SchoolContentEditor.java
@@ -0,0 +1,30 @@
+package se.idega.idegaweb.commune.school.presentation;
+
+import com.idega.block.school.data.School;
+import com.idega.presentation.IWContext;
+import com.idega.presentation.PresentationObject;
+import com.idega.presentation.text.Link;
+
+import java.rmi.RemoteException;
+/**
+ * @author gimmi
+ *
+ * To change this generated comment edit the template variable "typecomment":
+ * Window>Preferences>Java>Templates.
+ * To enable and disable the creation of type comments go to
+ * Window>Preferences>Java>Code Generation.
+ */
+public class SchoolContentEditor
+	extends com.idega.block.school.presentation.SchoolContentEditor {
+
+	protected com.idega.block.school.presentation.SchoolUserEditor getSchoolUserEditor(IWContext iwc) throws RemoteException {
+		return new SchoolUserEditor(iwc);
+	}
+
+	public static Link getLink(School school, PresentationObject po) throws RemoteException {
+		Link link = new Link(po);
+		link.setWindowToOpen( SchoolContentEditor.class);
+		link.addParameter(PARAMETER_SCHOOL_ID, school.getPrimaryKey().toString() );
+		return link;
+	}
+}
