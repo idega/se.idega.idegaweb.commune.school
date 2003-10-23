0752100b122a8188caeca953a431da821bf9142f
Thorhallur Helgason <laddi@idega.com>
2003-10-23 23:01:04 +0000

    Changed so block uses new method to find all cases for admin users.

--- src/java/se/idega/idegaweb/commune/presentation/AdminUserCases.java
+++ src/java/se/idega/idegaweb/commune/presentation/AdminUserCases.java
@@ -11,7 +11,6 @@ import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Collection;
 import java.util.Date;
-import java.util.Iterator;
 import java.util.List;
 import java.util.Vector;
 
