ec1890e0b4253a79b0b0e5178e8eeb332ea358db
Thorhallur Helgason <laddi@idega.com>
2003-11-06 08:39:03 +0000

    Changed so that it is not possible to choose a date back in time.

--- src/java/se/idega/idegaweb/commune/school/presentation/SchoolChoiceApplication.java
+++ src/java/se/idega/idegaweb/commune/school/presentation/SchoolChoiceApplication.java
@@ -847,7 +847,7 @@ public class SchoolChoiceApplication extends CommuneBlock {
 			IWTimestamp stamp = new IWTimestamp();
 			DateInput date = (DateInput) getStyledInterface(new DateInput(prmPlacementDate));
 			date.setDate(stamp.getDate());
-			//date.setEarliestPossibleDate(stamp.getDate(), iwrb.getLocalizedString("school.dates_back_in_time_not_allowed", "You can not choose a date back in time."));
+			date.setEarliestPossibleDate(stamp.getDate(), iwrb.getLocalizedString("school.dates_back_in_time_not_allowed", "You can not choose a date back in time."));
 			table.setHeight(row++, 5);
 			table.add(getSmallHeader(iwrb.getLocalizedString("school.placement_date", "Placement date")+":"), 1, row);
 			table.add(date, 3, row);
