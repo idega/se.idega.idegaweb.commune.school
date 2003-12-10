e99ef8be6ace1a4c48efa2cf91e4ba25fda43858
Goran Borgman <gobom@wmdata.com>
2003-12-10 15:39:54 +0000

    *** empty log message ***

---
+++ src/java/se/idega/idegaweb/commune/school/presentation/CentralPlacementEditLatestPlacement.java
@@ -0,0 +1,176 @@
+/*
+ * Created on 2003-sep-23
+ *
+ * To change the template for this generated file go to
+ * Window>Preferences>Java>Code Generation>Code and Comments
+ */
+package se.idega.idegaweb.commune.school.presentation;
+
+import java.rmi.RemoteException;
+
+import se.idega.idegaweb.commune.presentation.CommuneBlock;
+
+import com.idega.block.school.business.SchoolBusiness;
+import com.idega.block.school.data.SchoolClassMember;
+import com.idega.business.IBOLookup;
+import com.idega.idegaweb.IWResourceBundle;
+import com.idega.presentation.IWContext;
+import com.idega.presentation.PresentationObject;
+import com.idega.presentation.Table;
+import com.idega.presentation.text.Text;
+import com.idega.presentation.ui.Form;
+import com.idega.presentation.ui.HiddenInput;
+import com.idega.presentation.ui.SubmitButton;
+import com.idega.presentation.ui.TextInput;
+
+/**
+ * @author 
+ * @author <br><a href="mailto:gobom@wmdata.com">Göran Borgman</a><br>
+ * Last modified: $Date: 2003/12/10 15:40:04 $ by $Author: goranb $
+ * @version $Revision: 1.1 $
+ */
+public class CentralPlacementEditLatestPlacement extends CommuneBlock {
+	
+	// *** Localization keys ***
+	private static final String KP = "central_placement_floating_windows.";
+	private static final String KEY_WINDOW_HEADING = KP + "edit_latest_heading";
+	
+	// *** Params ***
+	public static final String PARAM_ACTION = "param_action";
+	public static final String PARAM_PLACEMENT_PARAGRAPH = "param_placement_paragraph";
+	public static final String PARAM_LATEST_PLACEMENT_ID = "param_latest_placement_id";
+	
+	public static final int ACTION_SAVE = 1;
+	public static final int ACTION_CANCEL = 2;
+
+	// Instance variables
+	private IWResourceBundle iwrb;
+	private Table mainTable;
+	private int mainTableRow;
+	private SchoolClassMember latestPl;
+	private int _action = -1;
+	private String paragraphTxt;
+
+	public void main(IWContext iwc) throws Exception {
+		iwrb = getResourceBundle(iwc);
+		parse(iwc);
+		latestPl = getPlacement(iwc);
+		if (_action == 1) { //ACTION_SAVE) {
+			save();			
+		}
+		Form form = new Form();
+		form.maintainParameter(PARAM_LATEST_PLACEMENT_ID);
+		mainTable = getMainTable();		
+		setMainTableContent(getEditTable());
+		form.add(mainTable);
+		add(form);
+	}
+
+	private Table getMainTable() {
+		mainTable = new Table();
+		mainTable.setBorder(0);
+		mainTable.setWidthAndHeightToHundredPercent();
+		mainTable.setCellpadding(0);
+		mainTable.setCellspacing(0);
+		int col = 1;
+		mainTableRow = 1;
+		
+		//  *** WINDOW HEADING ***
+		mainTable.add(getLocalizedSmallHeader(KEY_WINDOW_HEADING, "Edit Latest Placement"), col, mainTableRow);
+		mainTable.setColor(col, mainTableRow, getHeaderColor());
+		mainTable.setAlignment(col, mainTableRow, Table.HORIZONTAL_ALIGN_CENTER);
+		mainTable.setRowVerticalAlignment(mainTableRow, Table.VERTICAL_ALIGN_MIDDLE);
+		mainTable.setRowHeight(mainTableRow++, "20");
+
+		return mainTable;
+	}
+
+	private void setMainTableContent(PresentationObject obj) {
+		int col = 1;
+		mainTable.add(obj, col, mainTableRow++);
+	}
+
+	public Table getEditTable() {
+		Table table = new Table();
+		table.setBorder(1);
+		table.setWidthAndHeightToHundredPercent();
+		table.setRowHeight(1, "10");
+
+		table.setWidth(1, 1, "10");
+		table.setWidth(2, 1, "110");
+		table.setWidth(4, 1, "10");
+		table.setBorder(0);
+		table.setCellpadding(0);
+		table.setCellspacing(0);
+		table.setVerticalAlignment(2, 2, Table.VERTICAL_ALIGN_TOP);
+		
+		table.add(new HiddenInput(PARAM_ACTION, "1"), 1, 1);
+		
+		int row = 2;
+		int col = 2;
+		
+		table.add(
+				getSmallHeader(localize(CentralPlacementEditor.KEY_PLACEMENT_PARAGRAPH_LABEL, "Placement paragraph: ")),
+				col++, row);
+		table.add(getPlacementParagraphTextInput(), col, row);
+		row++;
+		table.setRowHeight(row, "20");
+		row++;
+		SubmitButton save =  new SubmitButton(iwrb.getLocalizedImageButton("central_placement_editor.button_save",
+													"Save"));
+		save.setValueOnClick(PARAM_ACTION, String.valueOf(ACTION_SAVE));
+		table.add(save, 2, row);
+		
+		table.add(Text.getNonBrakingSpace(2), 2, row);
+		
+		SubmitButton cancel =  new SubmitButton(iwrb.getLocalizedImageButton("central_placement_editor.button_cancel",
+						"Cancel"));
+		cancel.setValueOnClick(PARAM_ACTION, String.valueOf(ACTION_CANCEL));
+		table.add(cancel, 2, row);		
+		
+		// Last empty row
+		row++;
+		col = 2;
+		table.add(Text.getNonBrakingSpace(), col, row);
+		table.setRowHeight(table.getRows(), "100%");
+		
+		return table;
+	}
+	
+	private TextInput getPlacementParagraphTextInput() {
+		TextInput txt = (TextInput) getStyledInterface(new TextInput(PARAM_PLACEMENT_PARAGRAPH));
+		txt.setLength(25);
+		if (latestPl != null && latestPl.getPlacementParagraph() != null) {
+			txt.setContent(latestPl.getPlacementParagraph());
+		}
+		return txt;
+	}
+	
+	private void parse(IWContext iwc) {
+		_action = iwc.isParameterSet(PARAM_ACTION) ? Integer.parseInt(iwc.getParameter(PARAM_ACTION)) : -1;
+		paragraphTxt = iwc.isParameterSet(PARAM_PLACEMENT_PARAGRAPH) ? iwc.getParameter(PARAM_PLACEMENT_PARAGRAPH) : null;
+	}
+	
+	private void save() {
+		if (latestPl != null && paragraphTxt != null) {
+			latestPl.setPlacementParagraph(paragraphTxt);
+			latestPl.store();
+		}
+	}
+	
+	private SchoolClassMember getPlacement(IWContext iwc) {
+		SchoolClassMember plc = null;
+		if (iwc.isParameterSet(PARAM_LATEST_PLACEMENT_ID)) {
+			String idStr = iwc.getParameter(PARAM_LATEST_PLACEMENT_ID);
+			
+			try {
+				plc = getSchoolBusiness(iwc).getSchoolClassMemberHome().findByPrimaryKey(new Integer(idStr));
+			} catch (Exception e) {}
+		}
+		return plc;
+	}
+	
+	private SchoolBusiness getSchoolBusiness(IWContext iwc) throws RemoteException {
+		return (SchoolBusiness) IBOLookup.getServiceInstance(iwc, SchoolBusiness.class);
+	}	
+}
---
+++ src/java/se/idega/idegaweb/commune/school/presentation/CentralPlacementEditLatestPlacementWindow.java
@@ -0,0 +1,141 @@
+package se.idega.idegaweb.commune.school.presentation;
+
+import com.idega.idegaweb.IWResourceBundle;
+import com.idega.presentation.IWContext;
+import com.idega.presentation.Image;
+import com.idega.presentation.PresentationObject;
+import com.idega.presentation.Table;
+import com.idega.presentation.text.Text;
+import com.idega.presentation.ui.Form;
+import com.idega.presentation.ui.HiddenInput;
+import com.idega.presentation.ui.SubmitButton;
+import com.idega.presentation.ui.Window;
+
+/**
+ * @author borgman
+ *
+ * To change this generated comment edit the template variable "typecomment":
+ * Window>Preferences>Java>Templates.
+ * To enable and disable the creation of type comments go to
+ * Window>Preferences>Java>Code Generation.
+ */
+public class CentralPlacementEditLatestPlacementWindow extends Window {
+
+	private static final String IW_BUNDLE_NAME = "se.idega.idegaweb.commune";
+
+	// *** Localization keys ***
+	private static final String KP = "central_placement_floating_windows.";
+	private static final String KEY_WINDOW_HEADING_1 = KP + "edit_window_opened_from";
+	private static final String KEY_WINDOW_HEADING_2 = KP + "CENTRAL_PLACEMENT_EDITOR";
+	private static final String KEY_WINDOW_HEADING_RELOAD_MSG = KP + "close_reload_msg";
+	private static final String KEY_BUTTON_CLOSE_AND_RELOAD = KP + "button.close_and_reload";
+	private static final String PARAM_CLOSE_AND_RELOAD = "close_and_reload";
+
+	Table mainTable = null;
+	String FRAME_COLOR = "#DDDDDD";
+	IWResourceBundle iwrb = null;
+	 
+	// CSS styles   
+	private static final String STYLE_BIG_HEADER =
+		"font-style:normal;text-decoration:none;color:#000000;"
+			+ "font-size:12px;font-family:Verdana,Arial,Helvetica;font-weight:bold;";
+	private static final String STYLE_SMALL_HEADER =
+		"font-style:normal;text-decoration:none;color:#000000;"
+			+ "font-size:10px;font-family:Verdana,Arial,Helvetica;font-weight:normal;";
+	
+	public CentralPlacementEditLatestPlacementWindow() {
+		this.setWidth(400);
+		this.setHeight(250);
+		this.setScrollbar(true);
+		this.setResizable(true);	
+		this.setAllMargins(0);
+	}
+	
+	/*
+	 * @see com.idega.presentation.PresentationObject#getBundleIdentifier()
+	 */
+	public String getBundleIdentifier() {
+		return IW_BUNDLE_NAME;
+	}
+
+	private Table getMainTable() {
+		Table mainTable = new Table();
+		mainTable.setColumns(4);
+		mainTable.setRows(3);
+		mainTable.setWidthAndHeightToHundredPercent();
+		mainTable.setBorder(0);
+		mainTable.setCellpadding(2);
+		mainTable.setCellspacing(0);
+		int col = 1;
+		
+		// row 1
+		mainTable.setWidth(col, 1, "2px");
+		mainTable.setWidth(col+3, 1, "2px");
+		mainTable.setRowColor(1, FRAME_COLOR);
+		mainTable.setAlignment(col+1, 1, Table.HORIZONTAL_ALIGN_CENTER);
+		mainTable.setVerticalAlignment(col+1, 1, Table.VERTICAL_ALIGN_MIDDLE);
+		mainTable.setRowHeight(1, "20");
+		// row 2
+		mainTable.setColor(col, 2, FRAME_COLOR);
+		mainTable.mergeCells(col+1, 2, col+2, 2);
+		mainTable.setColor(col+3, 2, FRAME_COLOR);
+		// row 3		
+		mainTable.setRowColor(3, FRAME_COLOR);
+		mainTable.setRowHeight(3, "2px");
+
+		//  *** WINDOW HEADING ***
+		Text heading1 = new Text(iwrb.getLocalizedString(KEY_WINDOW_HEADING_1, 
+												"edit window opened from"));
+		Text heading2 = new Text(iwrb.getLocalizedString(KEY_WINDOW_HEADING_2, 
+												"Central Placement Editor"));
+		Text reloadMsg = new Text(iwrb.getLocalizedString(KEY_WINDOW_HEADING_RELOAD_MSG, 
+												"Press to close this window and reload Central Placement Editor"));
+									
+		heading1.setFontStyle(STYLE_SMALL_HEADER);
+		heading2.setFontStyle(STYLE_BIG_HEADER);
+		reloadMsg.setFontStyle(STYLE_SMALL_HEADER);
+		
+		mainTable.add(heading1, col+1, 1);
+		mainTable.add(Text.getBreak(), col+1, 1);
+		mainTable.add(heading2, col+1, 1);
+		
+		Image buttonImg = iwrb.getLocalizedImageButton(KEY_BUTTON_CLOSE_AND_RELOAD, 
+																			"Close and reload");
+		SubmitButton button = new SubmitButton(buttonImg);
+		Form closeAndReloadForm = new Form();
+		closeAndReloadForm.add(button);
+		closeAndReloadForm.add(new HiddenInput(PARAM_CLOSE_AND_RELOAD, "true"));
+		mainTable.add(closeAndReloadForm, col+2, 1);
+
+		return mainTable;
+	}
+	
+	private void setMainTableContent(PresentationObject obj) {
+		int col = 2;
+		int row = 2;
+		mainTable.add(obj, col, row);
+	}
+	
+	/**
+	 * Reload ParentPage and close this window
+	 */
+	private void submitParentAndClose() {
+		this.setParentPageFormToSubmitOnUnLoad(CentralPlacementEditor.FORM_NAME);
+		close();
+	}
+
+	/**
+	 * @see com.idega.presentation.PresentationObject#main(IWContext)
+	 */
+	public void main(IWContext iwc) throws Exception {
+		iwrb = getResourceBundle(iwc);
+		if (iwc.isParameterSet(PARAM_CLOSE_AND_RELOAD)) {
+			submitParentAndClose();
+		} else {
+			mainTable = getMainTable();
+			setMainTableContent(new CentralPlacementEditLatestPlacement());
+			add(mainTable);
+		}
+	}
+	
+}
