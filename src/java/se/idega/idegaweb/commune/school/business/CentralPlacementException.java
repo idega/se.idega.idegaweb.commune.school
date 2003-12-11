dd17d0c5cfd5d674c390e3eb7285087540016814
Goran Borgman <gobom@wmdata.com>
2003-12-11 14:31:07 +0000

    SchoolParentException renamed to SchoolException

--- src/java/se/idega/idegaweb/commune/school/business/CentralPlacementException.java
+++ src/java/se/idega/idegaweb/commune/school/business/CentralPlacementException.java
@@ -12,7 +12,7 @@ package se.idega.idegaweb.commune.school.business;
  * To change the template for this generated type comment go to
  * Window>Preferences>Java>Code Generation>Code and Comments
  */
-public class CentralPlacementException extends SchoolParentException {
+public class CentralPlacementException extends SchoolException {
   public CentralPlacementException(String key, String trans) {
     super(key, trans);
   }
