6258e88dccff84a924ebb463f62a8fe9263c6502
Thorhallur Helgason <laddi@idega.com>
2004-01-07 01:22:07 +0000

    Added two columns to available columns.

--- src/java/se/idega/idegaweb/commune/school/presentation/inputhandler/SchoolGroupColumnsHandler.java
+++ src/java/se/idega/idegaweb/commune/school/presentation/inputhandler/SchoolGroupColumnsHandler.java
@@ -49,6 +49,8 @@ public class SchoolGroupColumnsHandler extends SelectionBox implements InputHand
 		addMenuElement(SchoolReportBusiness.FIELD_PHONE, getLocalizedString(iwrb, SchoolReportBusiness.FIELD_PHONE, "Phone"));
 		addMenuElement(SchoolReportBusiness.FIELD_GENDER, getLocalizedString(iwrb, SchoolReportBusiness.FIELD_GENDER, "Gender"));
 		addMenuElement(SchoolReportBusiness.FIELD_LANGUAGE, getLocalizedString(iwrb, SchoolReportBusiness.FIELD_LANGUAGE, "Language"));
+		addMenuElement(SchoolReportBusiness.FIELD_SWEDISH_AS_SECOND_LANGUAGE, getLocalizedString(iwrb, SchoolReportBusiness.FIELD_SWEDISH_AS_SECOND_LANGUAGE, "Swedish as second language"));
+		addMenuElement(SchoolReportBusiness.FIELD_TERMINATION_DATE, getLocalizedString(iwrb, SchoolReportBusiness.FIELD_TERMINATION_DATE, "Termination date"));
 		addMenuElement(SchoolReportBusiness.FIELD_CUSTODIAN, getLocalizedString(iwrb, SchoolReportBusiness.FIELD_CUSTODIAN, "Custodian"));
 		addMenuElement(SchoolReportBusiness.FIELD_ALTERNATE_ADDRESS, getLocalizedString(iwrb, SchoolReportBusiness.FIELD_ALTERNATE_ADDRESS, "Alternate address"));
 		addMenuElement(SchoolReportBusiness.FIELD_YEARS_WITH_LANGUAGE, getLocalizedString(iwrb, SchoolReportBusiness.FIELD_YEARS_WITH_LANGUAGE, "Years with language"));
