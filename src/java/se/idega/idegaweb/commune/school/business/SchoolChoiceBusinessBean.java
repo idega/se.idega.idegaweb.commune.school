b962591477d7c862a511f99bfa024cf69c9e05f3
Malin Anulf <malin.anulf@agurait.com>
2004-01-27 14:04:38 +0000

    new line for spokesperson

--- src/java/se/idega/idegaweb/commune/school/business/SchoolChoiceBusinessBean.java
+++ src/java/se/idega/idegaweb/commune/school/business/SchoolChoiceBusinessBean.java
@@ -1569,7 +1569,7 @@ public class SchoolChoiceBusinessBean extends com.idega.block.process.business.C
 	private Chunk getReceiverChunk(SchoolChoiceReminderReceiver receiver) {
 		//String ssn = PersonalIDFormatter.format(receiver.getSsn(), getIWApplicationContext().getApplicationSettings().getDefaultLocale());
 		StringBuffer address = new StringBuffer();
-		address.append(getLocalizedString("school.spokesperson_for", "Spokesperson for")).append(": ");
+		address.append(getLocalizedString("school.spokesperson_for", "Spokesperson for")).append(": ").append("\n");
 		address.append(receiver.getStudentName()).append("\n"); 
 		address.append(receiver.getStreetAddress()).append("\n");
 		address.append(receiver.getPostalAddress()).append("\n");
