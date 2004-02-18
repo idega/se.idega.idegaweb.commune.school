1de05676d0a354cf53c8ee29234c695e63f8cc4a
Thorhallur Helgason <laddi@idega.com>
2004-02-18 08:49:51 +0000

    Changed earliest possible date to today + 1.

--- src/java/se/idega/idegaweb/commune/school/presentation/SchoolChoiceApplication.java
+++ src/java/se/idega/idegaweb/commune/school/presentation/SchoolChoiceApplication.java
@@ -977,6 +977,7 @@ public class SchoolChoiceApplication extends CommuneBlock {
 
 		if (_useOngoingSeason) {
 			IWTimestamp stamp = new IWTimestamp();
+			stamp.addDays(1);
 			DateInput date = (DateInput) getStyledInterface(new DateInput(prmPlacementDate));
 			date.setDate(stamp.getDate());
 			date.setEarliestPossibleDate(stamp.getDate(), iwrb.getLocalizedString("school.dates_back_in_time_not_allowed", "You can not choose a date back in time."));
