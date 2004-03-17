81aad2ade33684a6a14abddfc457dc04a46efb7d
Anders Lindman <anders.lindman@ncmedia.com>
2004-03-17 13:02:38 +0000

    First version.

---
+++ src/java/se/idega/idegaweb/commune/school/business/StudentAddressLabelsWriter.java
@@ -0,0 +1,219 @@
+/*
+ * $Id: StudentAddressLabelsWriter.java,v 1.1 2004/03/17 13:02:38 anders Exp $
+ *
+ * Copyright (C) 2003 Agura IT. All Rights Reserved.
+ *
+ * This software is the proprietary information of Agura IT AB.
+ * Use is subject to license terms.
+ *
+ */
+package se.idega.idegaweb.commune.school.business;
+
+import java.rmi.RemoteException;
+import java.util.Collection;
+import java.util.Iterator;
+
+import javax.ejb.FinderException;
+
+import se.idega.idegaweb.commune.business.CommuneUserBusiness;
+
+import com.idega.block.school.data.School;
+import com.idega.block.school.data.SchoolClass;
+import com.idega.block.school.data.SchoolClassMember;
+import com.idega.business.IBOLookup;
+import com.idega.core.file.data.ICFile;
+import com.idega.core.file.data.ICFileBMPBean;
+import com.idega.core.file.data.ICFileHome;
+import com.idega.core.location.data.Address;
+import com.idega.core.location.data.PostalCode;
+import com.idega.data.IDOLookupException;
+import com.idega.idegaweb.IWApplicationContext;
+import com.idega.idegaweb.IWMainApplication;
+import com.idega.io.MemoryFileBuffer;
+import com.idega.io.MemoryInputStream;
+import com.idega.io.MemoryOutputStream;
+import com.idega.user.data.User;
+import com.lowagie.text.Document;
+import com.lowagie.text.Font;
+import com.lowagie.text.PageSize;
+import com.lowagie.text.Phrase;
+import com.lowagie.text.Rectangle;
+import com.lowagie.text.pdf.PdfPCell;
+import com.lowagie.text.pdf.PdfPTable;
+import com.lowagie.text.pdf.PdfWriter;
+
+/** 
+ * This MediaWritable class generates a PDF stream with student address labels.
+ * <p>
+ * Last modified: $Date: 2004/03/17 13:02:38 $ by $Author: anders $
+ *
+ * @author Anders Lindman
+ * @version $Revision: 1.1 $
+ * @see com.idega.io.MediaWritable
+ */
+public class StudentAddressLabelsWriter {
+
+	private final static String REPORT_FOLDER_NAME = "Export Files";
+
+	private final static int NR_OF_COLUMNS = 3;
+	private final static int NR_OF_ROWS = 10;
+	private final static int NR_OF_ADDRESSES_PER_PAGE = NR_OF_COLUMNS * NR_OF_ROWS;	
+	private final static int ADDRESS_TABLE_WIDTH = 170;
+	private final static int ADDRESS_TABLE_HEIGHT = 80;
+	private final static int LEFT_MARGIN = 80;
+	private final static int TOP_START = 800;
+
+	private SchoolCommuneBusiness business;
+	private CommuneUserBusiness userBusiness;
+	private Font font = null;
+	private String filename = null;
+		
+	
+	/**
+	 * Creates the student address labels PDF file.
+	 */
+	public ICFile createFile(String[] schoolClassIds, IWMainApplication iwma) throws Exception {
+		ICFile reportFolder = null;
+		ICFileHome fileHome = null;
+
+		try {
+			fileHome = (ICFileHome) com.idega.data.IDOLookup.getHome(ICFile.class);
+			reportFolder = fileHome.findByFileName(REPORT_FOLDER_NAME);
+		} catch (FinderException e) {
+			try {
+				ICFile root = fileHome.findByFileName(ICFileBMPBean.IC_ROOT_FOLDER_NAME);
+				reportFolder = fileHome.create();
+				reportFolder.setName(REPORT_FOLDER_NAME);
+				reportFolder.setMimeType("application/vnd.iw-folder");
+				reportFolder.store();
+				root.addChild(reportFolder);
+			} catch (Exception e2) {
+				System.out.println(e2);
+				return null;
+			}
+		} catch (IDOLookupException e) {
+			System.out.println(e);
+			return null;
+		}
+
+		ICFile exportFile = null;
+		
+		try {
+			MemoryFileBuffer buffer = getPDFBuffer(schoolClassIds, iwma);
+			MemoryInputStream mis = new MemoryInputStream(buffer);
+
+			try {
+				exportFile = fileHome.findByFileName(filename);
+				if (exportFile != null) {
+					exportFile.remove();
+				}
+			} catch (FinderException e) {}
+
+			exportFile = fileHome.create();
+			exportFile.setFileValue(mis);
+			exportFile.setMimeType("application/pdf");
+			exportFile.setName(filename);
+			exportFile.setFileSize(buffer.length());
+			exportFile.store();
+			
+			reportFolder.addChild(exportFile);
+		} catch (Exception e) {
+			System.out.println(e);
+			throw e;
+		}
+		return exportFile;
+	}
+
+	/**
+	 * Creates PDF address labels for the specified school classes.   
+	 */
+	protected MemoryFileBuffer getPDFBuffer(String[] schoolClassIds, IWMainApplication iwma) throws Exception {
+		business = getSchoolCommuneBusiness(iwma.getIWApplicationContext());
+		userBusiness = getCommuneUserBusiness(iwma.getIWApplicationContext());
+
+		MemoryFileBuffer buffer = new MemoryFileBuffer();
+		MemoryOutputStream mos = new MemoryOutputStream(buffer);
+
+		Document document = new Document(PageSize.A4, 50, 50, 50, 50);
+		PdfWriter writer = PdfWriter.getInstance(document, mos);
+		document.addTitle("Student address labels");
+		document.addAuthor("Idega Reports");
+		document.addSubject("Student address labels");
+		document.open();
+		
+		font = new Font(Font.HELVETICA, 10, Font.BOLD);
+		
+		int studentCount = 0;
+
+		for (int i = 0; i < schoolClassIds.length; i++) {
+			SchoolClass schoolClass = business.getSchoolBusiness().findSchoolClass(new Integer(schoolClassIds[i]));
+			School school = schoolClass.getSchool();
+			filename = "student_address_labels_" + school.getPrimaryKey() + ".pdf";
+			Collection students = business.getSchoolBusiness().findStudentsInClass(((Integer)schoolClass.getPrimaryKey()).intValue());
+			Iterator iter = students.iterator();
+			while (iter.hasNext()) {
+				if (studentCount > 0 && studentCount % NR_OF_ADDRESSES_PER_PAGE == 0) {
+					document.newPage();
+				}
+				SchoolClassMember student = (SchoolClassMember) iter.next();
+				addAddress(writer, student, studentCount++);
+			}
+		}
+
+		if (studentCount == 0) {
+			throw new Exception("No students.");
+		}
+		
+		document.close();
+		writer.setPdfVersion(PdfWriter.VERSION_1_2);
+		buffer.setMimeType("application/pdf");
+		return buffer;
+	}
+	
+	/**
+	 * Adds a student address to the specified document. 
+	 */
+	protected void addAddress(PdfWriter writer, SchoolClassMember member, int studentCount) throws RemoteException {
+
+		User student = member.getStudent();
+		Address address = userBusiness.getUsersMainAddress(student);
+		PostalCode postalCode = address != null ? address.getPostalCode() : null;
+		
+		String name = student.getFirstName() + " " + student.getLastName();
+		String streetAddress = address != null ? address.getStreetAddress() : "";
+		String postalAddress = "";
+		if (address != null && postalCode != null) {
+			postalAddress = postalCode.getPostalAddress();
+		}
+
+		PdfPTable table = new PdfPTable(1);
+		table.setTotalWidth(ADDRESS_TABLE_WIDTH);
+		table.getDefaultCell().setPadding(3);
+		PdfPCell cell;
+
+		cell = new PdfPCell(new Phrase(name, font));
+		cell.setBorder(Rectangle.NO_BORDER);
+		table.addCell(cell);
+
+		cell = new PdfPCell(new Phrase(streetAddress, font));
+		cell.setBorder(Rectangle.NO_BORDER);
+		table.addCell(cell);
+
+		cell = new PdfPCell(new Phrase(postalAddress, font));
+		cell.setBorder(Rectangle.NO_BORDER);
+		table.addCell(cell);
+
+		int row = (studentCount / NR_OF_COLUMNS) % NR_OF_ROWS;
+		int column = studentCount % NR_OF_COLUMNS;
+		table.writeSelectedRows(0, -1, LEFT_MARGIN + column * ADDRESS_TABLE_WIDTH, 
+				TOP_START - row * ADDRESS_TABLE_HEIGHT, writer.getDirectContent());
+	}
+	
+	protected SchoolCommuneBusiness getSchoolCommuneBusiness(IWApplicationContext iwc) throws RemoteException {
+		return (SchoolCommuneBusiness) IBOLookup.getServiceInstance(iwc, SchoolCommuneBusiness.class);	
+	}
+
+	protected CommuneUserBusiness getCommuneUserBusiness(IWApplicationContext iwc) throws RemoteException {
+		return (CommuneUserBusiness) IBOLookup.getServiceInstance(iwc, CommuneUserBusiness.class);	
+	}
+}
---
+++ src/java/se/idega/idegaweb/commune/school/presentation/StudentAddressLabels.java
@@ -0,0 +1,209 @@
+/*
+ * $Id: StudentAddressLabels.java,v 1.1 2004/03/17 13:03:02 anders Exp $
+ *
+ * Copyright (C) 2003 Agura IT. All Rights Reserved.
+ *
+ * This software is the proprietary information of Agura IT AB.
+ * Use is subject to license terms.
+ *
+ */
+package se.idega.idegaweb.commune.school.presentation;
+
+import java.rmi.RemoteException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.List;
+
+import se.idega.idegaweb.commune.school.business.StudentAddressLabelsWriter;
+import se.idega.idegaweb.commune.school.event.SchoolEventListener;
+import se.idega.idegaweb.commune.school.presentation.inputhandler.SchoolGroupHandler;
+
+import com.idega.block.school.business.SchoolYearComparator;
+import com.idega.block.school.data.SchoolYear;
+import com.idega.core.file.data.ICFile;
+import com.idega.data.IDORelationshipException;
+import com.idega.presentation.ExceptionWrapper;
+import com.idega.presentation.IWContext;
+import com.idega.presentation.Table;
+import com.idega.presentation.text.Link;
+import com.idega.presentation.text.Text;
+import com.idega.presentation.ui.CheckBox;
+import com.idega.presentation.ui.Form;
+import com.idega.presentation.ui.GenericButton;
+import com.idega.presentation.ui.SubmitButton;
+
+/** 
+ * This idegaWeb block generates PDF files with student's addresses.
+ * <p>
+ * Last modified: $Date: 2004/03/17 13:03:02 $ by $Author: anders $
+ *
+ * @author Anders Lindman
+ * @version $Revision: 1.1 $
+ */
+public class StudentAddressLabels extends SchoolCommuneBlock {
+
+	private final static int ACTION_DEFAULT = 0;
+	private final static int ACTION_SELECT_SCHOOL_CLASSES = 1;
+	private final static int ACTION_CREATE_ADDRESS_LABELS = 2;
+
+	private final static String PP = "sal_"; // Parameter prefix
+	
+	private final static String PARAMETER_SELECT_SCHOOL_CLASSES = PP + "select_school_classes";
+	private final static String PARAMETER_SCHOOL_CLASS_IDS = PP + "school_class_ids";	
+	private final static String PARAMETER_CREATE_ADDRESS_LABELS = PP + "create_address_labels";
+	
+	private final static String KP = "sal."; // Key prefix
+	
+	private final static String KEY_STUDENT_ADDRESS_LABELS = KP + "student_address_labels";
+	private final static String KEY_BACK = KP + "back";
+	private final static String KEY_NO_STUDENTS = KP + "no_students";
+	
+	/**
+	 * @see se.idega.idegaweb.commune.school.presentation.SchoolCommuneBlock#init(com.idega.presentation.IWContext)
+	 */
+	public void init(IWContext iwc) throws Exception {
+		try {
+			int action = parseAction(iwc);
+			switch (action) {
+				case ACTION_DEFAULT:
+					handleDefaultAction();
+					break;
+				case ACTION_SELECT_SCHOOL_CLASSES:
+					handleSelectSchoolClassesAction();
+					break;
+				case ACTION_CREATE_ADDRESS_LABELS:
+					handleCreateAddressLabelsAction(iwc);
+					break;
+			}
+		} catch (Exception e) {
+			log(e);
+			add(new ExceptionWrapper(e, this));
+		}
+	}
+
+	/*
+	 * Returns the action constant for the action to perform based 
+	 * on the POST parameters in the specified context.
+	 */
+	private int parseAction(IWContext iwc) {
+		int action = ACTION_DEFAULT;
+
+		if (iwc.isParameterSet(PARAMETER_SELECT_SCHOOL_CLASSES)) {
+			action = ACTION_SELECT_SCHOOL_CLASSES;
+		} else if (iwc.isParameterSet(PARAMETER_CREATE_ADDRESS_LABELS)) {
+			action = ACTION_CREATE_ADDRESS_LABELS;
+		}
+
+		return action;
+	}
+	
+	/*
+	 * Handles the default action by displaying a form with season and year inputs. 
+	 */
+	private void handleDefaultAction() throws RemoteException {
+		Form form = new Form();
+		form.setEventListener(SchoolEventListener.class);
+
+		Table table = new Table();
+		table.setCellpadding(0);
+		table.setCellspacing(0);
+		table.setWidth(getWidth());
+		form.add(table);
+
+		int row = 1;
+		
+		List schoolYears = null;
+		try {
+			schoolYears = new ArrayList(getSession().getSchool().findRelatedSchoolYears());
+		}
+		catch (IDORelationshipException e1) {
+			schoolYears = new ArrayList();
+		}
+		if (!schoolYears.isEmpty()) {
+			Collections.sort(schoolYears, new SchoolYearComparator());
+		}
+		
+		table.add(getSmallHeader(localize("school.school_season", "Season") + ":"), 1, row);
+		table.add(Text.getNonBrakingSpace(), 1, row);
+		table.add(getSchoolSeasons(false), 1, row++);
+		table.setHeight(row++, 8);
+		
+		table.add(getSmallHeader(localize("school.school_years", "Years") + ":"), 1, row++);
+		table.add(getSmallHeader("&nbsp;"), 1, row++);
+		Iterator iter = schoolYears.iterator();
+		while (iter.hasNext()) {
+			SchoolYear year = (SchoolYear) iter.next();
+			CheckBox box = getCheckBox(getSession().getParameterSchoolGroupIDs(), year.getPrimaryKey().toString());
+			
+			table.setCellpadding(1, row, 2);
+			table.add(box, 1, row);
+			table.add(Text.getNonBrakingSpace(), 1, row);
+			table.add(getSmallText(year.getSchoolYearName()), 1, row++);
+		}
+		table.setHeight(row++, 15);
+		
+		SubmitButton button = (SubmitButton) getButton(new SubmitButton(PARAMETER_SELECT_SCHOOL_CLASSES, localize("school.continue", "Continue")));
+		button.setToEnableWhenChecked(getSession().getParameterSchoolGroupIDs());
+		
+		if (getBackPage() != null) {
+			GenericButton back = getButton(new GenericButton("back", localize("back", "Back")));
+			back.setPageToOpen(getBackPage());
+			
+			table.add(back, 1, row);
+			table.add(Text.getNonBrakingSpace(), 1, row);
+		}
+		table.add(button, 1, row++);
+
+		add(form);
+	}
+
+	/*
+	 * Views a form with selector for school classes. 
+	 */
+	private void handleSelectSchoolClassesAction() {
+		Form form = new Form();
+		SchoolGroupHandler sgh = new SchoolGroupHandler();
+		sgh.setName(PARAMETER_SCHOOL_CLASS_IDS);
+		form.add(sgh);
+
+		form.add(Text.getBreak());
+		
+		SubmitButton button = (SubmitButton) getButton(new SubmitButton(PARAMETER_CREATE_ADDRESS_LABELS, localize("school.continue", "Continue")));
+//		button.setToEnableWhenSelected(PARAMETER_SCHOOL_CLASS_IDS);
+		form.add(button);
+		
+		add(form);
+	}
+
+	/*
+	 * Creates a PDF file with student address labels with a link. 
+	 */
+	private void handleCreateAddressLabelsAction(IWContext iwc) {
+		String[] schoolClassIds = iwc.getParameterValues(PARAMETER_SCHOOL_CLASS_IDS);
+		Table table = new Table();
+		table.setCellpadding(getCellpadding());
+		table.setCellspacing(getCellspacing());
+
+		try {
+			StudentAddressLabelsWriter pdfWriter = new StudentAddressLabelsWriter();
+			ICFile file = pdfWriter.createFile(schoolClassIds, iwc.getIWMainApplication());
+			Link iconLink = new Link(getBundle().getImage("shared/pdf.gif"));
+			iconLink.setFile(file);
+			table.add(iconLink, 1, 1);
+			String title = localize(KEY_STUDENT_ADDRESS_LABELS, "Student address labels");
+			Link link = new Link(title);
+			link.setFile(file);
+			table.add(link, 2, 1);
+		} catch (Exception e) {
+			table.add(getErrorText(localize(KEY_NO_STUDENTS, "No students.")), 1, 1);
+		}		
+		Form form = new Form();
+		SubmitButton back = new SubmitButton("", localize(KEY_BACK, KEY_BACK));
+		back = (SubmitButton) getButton(back);
+		form.add(back);
+		table.add(form, 1, 4);
+		table.mergeCells(1, 4, 2, 4);
+		add(table);
+	}
+}
