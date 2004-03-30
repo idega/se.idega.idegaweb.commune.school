c529dcf41d84551a419ff9bddaaa46eba70d9938
Tryggvi Larusson <tryggvi.larusson@gmail.com>
2004-03-30 14:40:16 +0000

    Changes because of changes in iwc.getCurrentUser. Catching of NotLoggedOnException

--- src/java/se/idega/idegaweb/commune/school/presentation/SchoolContentItemEditorButton.java
+++ src/java/se/idega/idegaweb/commune/school/presentation/SchoolContentItemEditorButton.java
@@ -3,6 +3,8 @@ package se.idega.idegaweb.commune.school.presentation;
 import java.rmi.RemoteException;
 
 import com.idega.block.school.presentation.SchoolContentItem;
+import com.idega.core.accesscontrol.business.NotLoggedOnException;
+import com.idega.user.data.User;
 import com.idega.presentation.PresentationObject;
 import com.idega.presentation.text.Link;
 
@@ -14,17 +16,35 @@ public class SchoolContentItemEditorButton extends SchoolContentItem {
 	/**
 	 * @see com.idega.block.school.presentation.SchoolContentItem#getObject()
 	 */
-	protected PresentationObject getObject() throws RemoteException {
+	protected PresentationObject getObject() {
 //		System.out.println("SchoolContentItemEditButton : hasContentEdit = "+super.getSchoolContentBusiness(_iwc).hasEditPermission(_school, super._iwc));
 		
 		
 		Link link = SchoolContentEditor.getLink(_school, _iwrb.getLocalizedImageButton("content_editor","Content Editor"));
-
-		if (super.hasEditPermission()) {
-			return link;
-		}else if (super.getSchoolBusiness(_iwc).hasEditPermission(_iwc.getCurrentUser(), _school)) {
+		
+		if (hasEditPermission()) {
 			return link;
 		}
 		return null;
+	}
+	
+	public boolean hasEditPermission(){
+		if(super.hasEditPermission()){
+			return true;
+		}
+		else{
+			try{
+				User user = _iwc.getCurrentUser();
+				return super.getSchoolBusiness(_iwc).hasEditPermission(user, _school);
+			}
+			catch(NotLoggedOnException nle){
+				
+			}
+			catch (RemoteException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		return false;
 	}
 }
