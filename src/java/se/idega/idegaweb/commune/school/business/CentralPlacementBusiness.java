62e0252f7101572d438143d2240c2d631dc0b048
Thomas Hilbig <thomas@idega.com>
2004-10-15 14:45:12 +0000

    moved store method of CentralPlacementEditor to school.accounting bundle

--- src/java/se/idega/idegaweb/commune/school/business/CentralPlacementBusiness.java
+++ src/java/se/idega/idegaweb/commune/school/business/CentralPlacementBusiness.java
@@ -12,5 +12,4 @@ public interface CentralPlacementBusiness extends com.idega.business.IBOService
  public se.idega.idegaweb.commune.school.business.SchoolChoiceBusiness getSchoolChoiceBusiness()throws java.rmi.RemoteException, java.rmi.RemoteException;
  public com.idega.user.business.UserBusiness getUserBusiness()throws java.rmi.RemoteException, java.rmi.RemoteException;
  public void removeSchoolClassMember(java.lang.Integer p0) throws java.rmi.RemoteException;
- public com.idega.block.school.data.SchoolClassMember storeSchoolClassMember(com.idega.presentation.IWContext p0,int p1)throws java.rmi.RemoteException,se.idega.idegaweb.commune.school.business.CentralPlacementException, java.rmi.RemoteException;
 }
