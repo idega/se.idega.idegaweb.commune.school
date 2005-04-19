36807590525471d7fa7dd29299d84dd622b2a8bd
Thorhallur Helgason <laddi@idega.com>
2005-04-19 13:18:40 +0000

    *** empty log message ***

--- src/java/se/idega/idegaweb/commune/school/presentation/EventList.java
+++ src/java/se/idega/idegaweb/commune/school/presentation/EventList.java
@@ -1,6 +1,7 @@
 package se.idega.idegaweb.commune.school.presentation;
 
 import java.rmi.RemoteException;
+import java.util.ArrayList;
 import java.util.Collection;
 
 import javax.ejb.FinderException;
@@ -17,7 +18,7 @@ import com.idega.presentation.IWContext;
  * Copyright:    Copyright idega Software (c) 2002
  * Company:	idega Software
  * @author <a href="mailto:roar@idega.is">roar</a>
- * @version $Id: EventList.java,v 1.19 2004/06/10 15:15:11 gimmi Exp $
+ * @version $Id: EventList.java,v 1.20 2005/04/19 13:18:40 laddi Exp $
  * @since 17.3.2003 
  */
 // refactored by aron 16.04.2004
@@ -41,13 +42,16 @@ public class EventList extends PrintMessageViewer {
 			if (ssn != null) {
 				ssn.replaceAll("-", "");
 			}
-			return getPrintedLetter().
+			Collection collection = new ArrayList();
+			collection.addAll(getPrintedLetter().
 			findAllLettersBySchool(
 						schoolID,
 						ssn,
 						getSearchMsgId(),
 						getUFrom(),
-						getUTo());
+						getUTo()));
+			collection.addAll(getPrintedLetter().findLettersByChildcare(schoolID, ssn, getSearchMsgId(), getUFrom(), getUTo()));
+			return collection;
 		}
 		catch (RemoteException e) {
 			throw new FinderException(e.getMessage());
