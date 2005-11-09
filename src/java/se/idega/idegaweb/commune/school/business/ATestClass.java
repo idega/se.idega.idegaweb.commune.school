ee00df7302b70c1d7f07d701e79204b473a8bfa5
Dainis Brjuhoveckis <dainis.brjuhoveckis@idega.lv>
2005-11-09 16:33:54 +0000

    testing merge with BRANCH_PLATFORM_2 stuff

---
+++ src/java/se/idega/idegaweb/commune/school/business/ATestClass.java
@@ -0,0 +1,13 @@
+package se.idega.idegaweb.commune.school.business;
+
+public class ATestClass {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
