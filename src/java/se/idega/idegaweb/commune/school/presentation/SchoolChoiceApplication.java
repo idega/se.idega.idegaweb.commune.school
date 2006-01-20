b587b7c679be1a7e87fba160745e9eb21a545f73
Dainis Brjuhoveckis <dainis.brjuhoveckis@idega.lv>
2006-01-20 14:31:27 +0000

    removed unused imports

--- src/java/se/idega/idegaweb/commune/school/presentation/SchoolChoiceApplication.java
+++ src/java/se/idega/idegaweb/commune/school/presentation/SchoolChoiceApplication.java
@@ -8,7 +8,6 @@ import java.text.MessageFormat;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.Date;
-import java.util.GregorianCalendar;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
